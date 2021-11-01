3ccbe588a189851125e955025adc3dfc062d7f27
John Fiifi Kwapong <john@senseitechnology.com>
2021-11-01 18:48:30 +0000

    about now works

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template      
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("home.html")
     
+@app.route("/salvador")
+def salvador():
+    return "Hello, Salvador"
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000)
+    app.run(debug=True)
